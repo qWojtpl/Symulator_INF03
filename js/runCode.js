b9a7a3bf8135617a7676e2c06cf9870fb2697231
qWojtpl <wojtoo24@gmail.com>
2025-01-20 23:51:11 +0100

    Plain HTML documents
    - Smaller CSS logo
    - Added JS logo
    - Added HTML logo
    - Added support for plain HTML documents

--- js/runCode.js
+++ js/runCode.js
@@ -22,7 +22,14 @@ function buttonClick() {
         return;
     }
     saveCurrentFile();
-    simulate(document.getElementById("output").contentDocument, getEditorCode(document.getElementById("code-editor")));
+    let name = getCurrentFileName();
+    let contentDocument = document.getElementById("output").contentDocument;
+    let editorCode = getEditorCode(document.getElementById("code-editor"));
+    if(name.endsWith("html")) {
+        setOutput(contentDocument, editorCode);
+    } else if(name.endsWith("php")) { 
+        simulate(contentDocument, editorCode);
+    }
 }
 
 function simulate(contentDocument, code) {
@@ -34,31 +41,35 @@ function simulate(contentDocument, code) {
 
     xhr.open("POST", "../lib/simulate.php", true);
     xhr.onload = function () {
-        contentDocument.open();
-        let parser = new DOMParser();
-        let newDocument = parser.parseFromString(this.responseText, "text/html");
-        let scripts = newDocument.querySelectorAll("script[src]");
-        for(let i = 0; i < scripts.length; i++) {
-            let script = scripts[i];
-            let source = script.getAttribute("src");
-            script.removeAttribute("src");
-            script.innerText = getFile(EXAM_NAME + source);
-        }
-        let styles = newDocument.querySelectorAll("link[rel=stylesheet]");
-        for(let i = 0; i < styles.length; i++) {
-            let style = styles[i];
-            let source = style.getAttribute("href");
-            style.removeAttribute("href");
-            style.setAttribute("ref", source);
-            let head = newDocument.querySelector("head");
-            let styleElement = newDocument.createElement("style");
-            styleElement.innerText = parser.parseFromString(getFile(EXAM_NAME + source), "text/html").documentElement.textContent;
-            head.appendChild(styleElement);
-        }
-        contentDocument.write(newDocument.documentElement.innerHTML);
-        contentDocument.close();
+        setOutput(contentDocument, this.responseText);
     };
 
     xhr.send(data);
 
+}
+
+function setOutput(contentDocument, outputCode) {
+    contentDocument.open();
+    let parser = new DOMParser();
+    let newDocument = parser.parseFromString(outputCode, "text/html");
+    let scripts = newDocument.querySelectorAll("script[src]");
+    for(let i = 0; i < scripts.length; i++) {
+        let script = scripts[i];
+        let source = script.getAttribute("src");
+        script.removeAttribute("src");
+        script.innerText = parser.parseFromString(getFile(EXAM_NAME + source), "text/html").documentElement.textContent;
+    }
+    let styles = newDocument.querySelectorAll("link[rel=stylesheet]");
+    for(let i = 0; i < styles.length; i++) {
+        let style = styles[i];
+        let source = style.getAttribute("href");
+        style.removeAttribute("href");
+        style.setAttribute("ref", source);
+        let head = newDocument.querySelector("head");
+        let styleElement = newDocument.createElement("style");
+        styleElement.innerText = parser.parseFromString(getFile(EXAM_NAME + source), "text/html").documentElement.textContent;
+        head.appendChild(styleElement);
+    }
+    contentDocument.write(newDocument.documentElement.innerHTML);
+    contentDocument.close();
 }
