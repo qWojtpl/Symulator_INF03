85b2cff86a40af26ecee26b88b7d4ee840c85487
qWojtpl <wojtoo24@gmail.com>
2025-03-29 02:03:50 +0100

    Added result messages
    - Testing results for CSS
    - Results styles
    - Result messages

--- js/examChecker.js
+++ js/examChecker.js
@@ -2,10 +2,12 @@
 init();
 
 let answerKey = [];
+let answerKeyMessages = [];
 let result = [];
 
 function init() {
     downloadExamAnswerKey();
+    downloadExamAnswerKeyMessages();
 }
 
 function checkExam() {
@@ -20,15 +22,27 @@ function checkExam() {
     document.getElementById("virtual-code-dumpster").innerHTML = "";
     let examSummary = document.getElementById("exam-summary");
     for(let i = 0; i < result.length; i++) {
-        let span = document.createElement("span");
-        span.innerHTML = JSON.stringify(result[i]);
-        examSummary.appendChild(span);
+        let div = document.createElement("div");
+        div.classList.add("exam-summary-result");
+        let sign = document.createElement("span");
+        if(result[i].type == "ERROR") {
+            sign.innerText = "X";
+            sign.classList.add("error");
+        } else if(result[i].type == "RESULT") {
+            sign.innerText = "✓";
+            sign.classList.add("ok");
+        }
+        div.appendChild(sign);
+        let message = document.createElement("span");
+        message.innerText = answerKeyMessages[result[i].index];
+        div.appendChild(message);
+        examSummary.appendChild(div);
     }
 }
 
 function checkExamFile(fileName, index) {
     if(!isFileExists(EXAM_NAME + fileName)) {
-        addCheckExamError("FILE_NOT_FOUND", fileName);
+        addCheckExamError("FILE_NOT_FOUND", fileName, index);
         return;
     }
     let dumpster = document.getElementById("virtual-code-dumpster");
@@ -53,11 +67,11 @@ function checkExamFile(fileName, index) {
         if(answerKey[i].startsWith("@")) {
             continue;
         }
-        checkLine(answerKey[i], contentDocument);
+        checkLine(answerKey[i], contentDocument, i);
     }
 }
 
-function checkLine(line, contentDocument) {
+function checkLine(line, contentDocument, index) {
     if(line == "\r\n" || line == "" || line == "\n") {
         return;
     }
@@ -73,7 +87,6 @@ function checkLine(line, contentDocument) {
         }
         if(contentSplit[0].includes(" main$")) {
             let b = contentSplit[0].replaceAll(" main$", " main");
-            b += ", " + contentSplit[0].replaceAll(" main$", " main");
             b += ", " + contentSplit[0].replaceAll(" main$", " section");
             contentSplit[0] = b;
         }
@@ -98,31 +111,49 @@ function checkLine(line, contentDocument) {
             negation = true;
             contentSplit[0] = contentSplit[0].replaceAll("!", "");
         }
-        let elements = contentDocument.querySelectorAll(contentSplit[0]);
+        let cssStyle = false;
+        if(contentSplit[0].startsWith("=")) {
+            cssStyle = true;
+            contentSplit[0] = contentSplit[0].substring(1);
+        }
+        let elements;
+        if(contentSplit[0].startsWith("*") && cssStyle) {
+            let dummy = document.createElement("dummy");
+            contentDocument.querySelector("body").appendChild(dummy);
+            elements = [dummy];
+        } else {
+            elements = contentDocument.querySelectorAll(contentSplit[0]);
+        }
         if(elements.length > 0 && negation) {
-            addCheckExamError("Element which is not allowed", line);
             continue;
         } else if(negation) {
             c++;
             continue;
         }
         if(elements.length == 0) {
-            addCheckExamError("ELEMENT NOT FOUND", line);
             continue;
         }
         for(let j = 0; j < elements.length; j++) {
             let element = elements[j];
             if(contentSplit.length > 1) {
                 let content = contentSplit[1];
-                if(content.startsWith("s")) {
-                    content = content.substring(2);
-                    if(!element.innerHTML.startsWith(content)) {
+                if(cssStyle) {
+                    if(window.getComputedStyle(element)["fontFamily"] != "Cambria") {
                         continue;
                     }
                 } else {
-                    content = content.substring(1);
-                    if(element.innerHTML != content) {
-                        continue;
+                    if(content.startsWith("s")) {
+                        content = content.substring(2);
+                        if(!element.innerHTML.startsWith(content)) {
+                            continue;
+                        }
+                    } else {
+                        content = content.substring(1);
+                        let currentContent = element.innerHTML;
+                        currentContent = currentContent.replaceAll("\t", "");
+                        if(currentContent != content) {
+                            continue;
+                        }
                     }
                 }
             }
@@ -131,29 +162,34 @@ function checkLine(line, contentDocument) {
         }
     }
     if(c != selectors.length) {
-        addCheckExamCallback("NOT_MEET", line);
+        addCheckExamError("NOT_MEET", index);
+    } else {
+        addCheckExamResult("RESULT_OK", index);
     }
 } 
 
-function addCheckExamError(name, message) {
-    console.log("Error: " + name + " " + message);
-    addCheckExamCallback(name, message, "ERROR");
+function addCheckExamError(name, index) {
+    addCheckExamCallback(name, index, "ERROR");
 }
 
-function addCheckExamResult(name, message) {
-    addCheckExamCallback(name, message, "RESULT");
+function addCheckExamResult(name, index) {
+    addCheckExamCallback(name, index, "RESULT");
 }
 
-function addCheckExamCallback(name, message, type) {
+function addCheckExamCallback(name, index, type) {
     result[result.length] = {
         name: name,
-        message: message,
+        index: index,
         type: type
     }
 }
 
 function clearCheckExamResult() {
     result = [];
+    let elements = document.querySelectorAll("#exam-summary div.exam-summary-result, #exam-summary dummy");
+    for(let i = 0; i < elements.length; i++) {
+        elements[i].remove();
+    }
 }
 
 function downloadExamAnswerKey() {
@@ -161,6 +197,15 @@ function downloadExamAnswerKey() {
     xhr.open("GET", "../assets/" + EXAM_NAME + "/exam.key", true);
     xhr.onload = function () {
         answerKey = this.responseText.split("\r\n");
+    };
+    xhr.send();
+}
+
+function downloadExamAnswerKeyMessages() {
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "../assets/" + EXAM_NAME + "/exam-messages.key", true);
+    xhr.onload = function () {
+        answerKeyMessages = this.responseText.split("\r\n");
     };
     xhr.send();
 }
